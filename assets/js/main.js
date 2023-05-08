2596b8245fee2d275ef0260ac0ee803d5082753d
Kervin Espirito Santo <57775586+kervines@users.noreply.github.com>
2023-05-07 23:01:40 -0300

    adicionado cores para cada tipo de resultado de imc

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,57 @@
 function mainProject() {
-  const form = document.querySelector("#form");
-  const result = document.querySelector(".result");
+  const form = document.querySelector('#form');
+  const result = document.querySelector('.result');
 
   function calcularImc(event) {
     event.preventDefault();
-    const peso = form.querySelector("#input-weigth");
-    const altura = form.querySelector("#input-heigth");
+    const peso = form.querySelector('#input-weigth');
+    const altura = form.querySelector('#input-heigth');
 
     const imc =
       parseFloat(peso.value) / parseFloat(altura.value * altura.value);
-
     let resultado;
 
-    console.log(imc.toFixed(2));
-    console.log(typeof imc);
-
-    if (peso.value === "" || altura.value === "") {
+    if (peso.value === '' || altura.value === '') {
       result.innerHTML = `<p class="negado">Preencha todos os dados.</p >`;
     } else if (Number.isNaN(imc)) {
       result.innerHTML = `<p class="negado">Valores inválidos, tente novamente!</p >`;
     } else {
       if (imc < 18.5) {
-        resultado = "Abaixo do peso";
+        resultado = 'Abaixo do peso';
+        result.innerHTML = `<p class="anormal">Seu IMC é ${imc.toFixed(
+          2
+        )}. Situação: ${resultado}</p>`;
       } else if (imc >= 18.5 && imc < 24.9) {
-        resultado = "Peso normal";
+        resultado = 'Peso normal';
+        result.innerHTML = `<p class="aceito">Seu IMC é ${imc.toFixed(
+          2
+        )}. Situação: ${resultado}</p>`;
       } else if (imc >= 25 && imc < 29.9) {
-        resultado = "Acima do peso";
+        resultado = 'Acima do peso';
+        result.innerHTML = `<p class="anormal">Seu IMC é ${imc.toFixed(
+          2
+        )}. Situação: ${resultado}</p>`;
       } else if (imc >= 30 && imc < 34.9) {
-        resultado = "Obesidade 1";
+        resultado = 'Obesidade 1';
+        result.innerHTML = `<p class="anormal-1">Seu IMC é ${imc.toFixed(
+          2
+        )}. Situação: ${resultado}</p>`;
       } else if (imc >= 35 && imc < 39.9) {
-        resultado = "Obesidade 2";
+        resultado = 'Obesidade 2';
+        result.innerHTML = `<p class="anormal-1">Seu IMC é ${imc.toFixed(
+          2
+        )}. Situação: ${resultado}</p>`;
       } else {
-        resultado = "Obesidade 3";
+        resultado = 'Obesidade 3';
+        result.innerHTML = `<p class="anormal-2">Seu IMC é ${imc.toFixed(
+          2
+        )}. Situação: ${resultado}</p>`;
       }
-      result.innerHTML = `<p class="aceito">Seu IMC é ${imc.toFixed(
-        2
-      )}. Situação: ${resultado}</p>`;
+      // result.innerHTML = `<p class="aceito">Seu IMC é ${imc.toFixed(
+      //   2
+      // )}. Situação: ${resultado}</p>`;
     }
   }
-  form.addEventListener("submit", calcularImc);
+  form.addEventListener('submit', calcularImc);
 }
 mainProject();
